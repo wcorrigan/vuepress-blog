068e62715c4f9e2ed65ded02206801b80da11355
Bill Corrigan <bill@bcorrigan.com>
2021-12-27 23:36:45 +0000

    some content added

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: "Front-end Web School",
+    description: "Learn HTML, CSS, and JavaScript",
+    head: [
+      [
+        "link",
+        {
+          rel: "stylesheet",
+          href:
+            "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+        }
+      ],
+      [
+        "link",
+        {
+          rel: "stylesheet",
+          href: "https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+        }
+      ]
+    ]
+  };
